c69462df70973e6eba02e832c866647f3344dd55
N-Mohamad <101737216+N-Mohamad@users.noreply.github.com>
2023-10-02 17:16:06 +0200

    Update App.js
    spaces and numbers

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
 import { useState } from "react";
 
+
 function App() {
   const [count, setCount] = useState("0");
   const [text, setText] = useState ("");
+  const [spaces, setSpaces] = useState(false); 
+  const [numbers, setNumbers] = useState(false); 
+
 
   const inputHandler = (event) => {
     setText(event.target.value);
   };
 
   const countAll = () =>{
-    setCount(text.length)
+
+    let checkText = text;
+    if (!spaces){
+           checkText = checkText.replace(/\s/g, "");
+
+    }
+    if(!numbers){
+           checkText = checkText.replace(/\d/g, "");
+
+    }
+    setCount(checkText.length)
   }
 
+  const handleSpacesCheckbox = () => {
+    setSpaces(!spaces);
+  }
 
-  return (
+  const handleNumbersCheckbox = () => {
+    setNumbers(!numbers);
+  }
+
+  return (      
+  <div>
+
     <div>
       <h4>Collez votre texte ici:</h4>
-      <textarea maxLength="10" cols="100" rows="18" onChange={inputHandler} />
+      <textarea maxLength="20" cols="100" rows="18" onChange={inputHandler} />
       <div>
+
+        <input type="checkbox" checked={spaces} onChange={handleSpacesCheckbox}/>
+        <label>spaces</label>
+        <input type="checkbox" checked={numbers} onChange={handleNumbersCheckbox}/>
+        <label>numbers</label>
         <button onClick={countAll} color="blue">Comptez</button>
         <h6>{count} characters </h6>      
 
       </div>    
 
     </div>
-
+</div>
   );
 }
 
